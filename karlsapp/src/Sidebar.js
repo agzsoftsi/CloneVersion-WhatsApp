e24bbca1c7af1d560a9ba80ea80734d520deefbe
Carlos Garcia <agzsoftsi@gmail.com>
2021-01-31 21:34:34 -0500

    Icons Top on Sidebar created

--- karlsapp/src/Sidebar.js
+++ karlsapp/src/Sidebar.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 import './Sidebar.css';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
+                <Avatar />
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                    <MoreVertIcon />  
+                    </IconButton>
+                 
+                </div>
 
             </div>
             <div className="sidebar_search">
