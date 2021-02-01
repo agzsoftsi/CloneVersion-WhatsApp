9a7115bf763dd569d3a0fb85d7cf222bad82d6ae
Carlos Garcia <agzsoftsi@gmail.com>
2021-02-01 17:02:20 -0500

    Name of rooms is on side chat

--- karlsapp/src/Chat.js
+++ karlsapp/src/Chat.js
@@ -6,11 +6,22 @@ import MicIcon from '@material-ui/icons/Mic';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import "./Chat.css"
 import { useParams } from 'react-router-dom';
+import db from './firebase';
 
 function Chat() {
     const [input, setInput] = useState("");
     const [seed, setSeed] = useState('');
     const { roomId } = useParams();
+    const [roomName, setRoomName] = useState('');
+
+    useEffect(() => {
+       if(roomId){
+           db.collection('rooms')
+           .doc(roomId)
+           .onSnapshot((snapshop) => setRoomName
+           (snapshop.data().name));
+       }
+    }, [roomId]);
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000));
@@ -28,7 +39,7 @@ function Chat() {
              <div className="chat_header">
             <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`}/>
                 <div className="chat_headerInfo">
-                    <h3>Room name</h3>
+                    <h3>{roomName}</h3>
                     <p>Lasst seen At ...</p>
                 </div>
                 <div className="chat_headerRight">
